ca6b73d26f532b35f75d274788ae47a6afffe60b
Blair V <blair.vanderlugt@noaa.gov>
2014-12-03 09:18:46 -0800

    data was randomized using runif for example purposes

--- external/shinyFooter.R
+++ external/shinyFooter.R
@@ -1,12 +1,15 @@
 HTML('<div class = "modal-footer">
         <div class = "footerlinks">
-          <a href="http://www.nmfs.noaa.gov/">NMFS</a>
+          <a href="http://www.nwfsc.noaa.gov/">NWFSC</a>
+          <a href="http://www.nmfs.noaa.gov/">NOAA Fisheries</a>
           <a href="http://www.noaa.gov/">NOAA</a>
-          <a href="http://www.commerce.gov/">DOC</a>
-          <a href="http://www.noaa.gov/privacy.html">Privacy Policy</a>
+          <a href="http://www.nwfsc.noaa.gov/copyright.cfm">Copyright policy</a>
+          <a href="http://www.nmfs.noaa.gov/aboutus/privacy.html">Privacy policy</a>
           <a href="https://nwfsc.noaa.gov/disclaimer.cfm">Disclaimer</a>
-          <a href="https://nwfsc.noaa.gov/accessibility.cfm">Accessibility</a>
-          <a href="mailto:blair.vanderlugt@noaa.gov">Contact</a>
+          <a href="http://www.nwfsc.noaa.gov/contact/feedback.cfm">Feedback</a>
+          <a href="http://explorer.iugo-cafe.org/framedemo/footer.html">NOAA customer satisfaction survey</a>
+          <a href="http://www.cio.noaa.gov/services_programs/info_quality.html">NOAA information quality</a>
+          <a href="mailto:blair.vanderlugt@noaa.gov">Contact Us</a>
         </div>
   </div>'
 )
