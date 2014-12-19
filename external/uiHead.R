b328a7b1f7d87b1f31425f159a7214b0c485037e
unknown <Blair.Vanderlugt@NWCDW01697885.nmfs.local>
2014-12-19 11:14:09 -0800

    css styles updated, moved ui header/footer to uiComponents

--- external/uiHead.R
+++
@@ -1,2 +0,0 @@
-# custom css functions
-wellPanelSub <- function(...){div(class = "well-sub", ...)} # calls .css selector for well-sub
--- ui.R
+++ ui.R
@@ -1,19 +1,23 @@
 
 # custom css functions
-source("external/uiHead.R")
+wellPanelSub <- function(...){div(class = "well-sub", ...)} # calls .css selector for well-sub
 
-#UI
+
+# UI
+# fluidPage(
+#     source("external/uiHead.R", local = TRUE)$value,
+#     source("external/explorer/ex.fluidPage.r", local = TRUE)$value
+# )
+
+
+# OG UI
 shinyUI(
   navbarPage(title = "FISHeries Economics Explorer (FISHEyE)",
-             tabPanel(title = "Home", source("external/about.r", local = TRUE)$value),
-             navbarMenu(title = "Economic Indicators",                   
-                        tabPanel(title = "Net Revenue", source("external/explorer/ex.fluidPage.r", local = TRUE)$value),
-                        tabPanel(title = "Costs", source("external/costs/costs.fluidPage.R")$value)
-             ),
-             navbarMenu("Scenario Planning",
-                        tabPanel("Net Rev Simulator", source("external/simulator/sim.fluidPage.r", local = TRUE)$value)             
-             ),
-             theme = "bootstrapFisheye.css", #note that you need to change theme both here and in the fluidpage
-             footer = source("external/shinyFooter.R", local = TRUE)$value
+             header = source("external/uiComponents/uiHead.R", local = TRUE)$value,                   
+             tabPanel(title = "Net Revenue", source("external/explorer/ex.fluidPage.r", local = TRUE)$value),
+             tabPanel(title = "Costs", source("external/costs/costs.fluidPage.R", local = TRUE)$value),
+             theme = "bootstrap_nwfsc.css",
+             footer = source("external/uiComponents/uiFooter.R", local = TRUE)$value
+             
   ) 
 )
