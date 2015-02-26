39adea5ad453644aea7b438966b9b334b7f7bf99
Blair Vanderlugt <blair.vanderlugt@noaa.gov>
2015-02-26 14:49:34 -0800

    added instructions to thirds

--- external/explorer/explorerSourceFiles/defaultText.R
+++ external/explorer/explorerSourceFiles/defaultText.R
@@ -18,14 +18,17 @@ output$DefaultPlotText <- renderUI({
 output$DefaultThirdsText <- renderUI({
   if(PermitPlot()) return()
   tags$div(style = "margin-top: 15px",
-    p("Plot the  
-      WestCoast Groundfish fishery."),
+    p("Plot the variation in performance within a summary variable."),
+    p("Vessels are ordered by Revenue and reported as groups of three vessels
+      for the highest revenue, middle revenue and lowest revenue.")
     tags$ol(
       tags$li("Choose data in the 'Select Data' pane"),
       tags$li("Plot and tabular output can be found under their respective tabs"),
       tags$li("Plot options are found in the pane below the output"),
       tags$li("Plot and table download buttons can be found in the Plot Options
-              pane")
+              pane"),
+      tags$li(tags$strong("Currently only supports one summary variable selection")),
+      tags$li(tags$strong("Currently only supports the mean summary statistic"))
     )
   )    
 })
