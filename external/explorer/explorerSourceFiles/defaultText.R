1df03cf50dbaf5476c59c5064283fd34135adc10
Melanie <melanie.harsch@NWCDW01697885.nmfs.local>
2015-07-24 14:55:06 -0700

    Added text re: catch shares program and dashed lines in plots.  Some restructuring.

--- external/explorer/explorerSourceFiles/defaultText.R
+++ external/explorer/explorerSourceFiles/defaultText.R
@@ -5,8 +5,14 @@ output$DefaultPlotText <- renderUI({
   if(PermitPlot()) return()
   tags$div(style = "margin: 15px 15px 30px; width: 60%",
            tags$h4("Plot data"),
-           tags$p("Visualize summary statistics for net revenue of West Coast Catcher Vessels.",tags$br(),
-                  "To get started, make at least one selection in each of the panels on the left.",tags$br(),
+           tags$p("Visualize summary statistics for net revenue of West Coast Catcher Vessels. ",tags$br(),
+                  'With the "bar" graph option, you can compare selected economic measures side-by-sde, view the "Total Cost Net Revenue" figure (economic measures pre-defined),
+                  or view the "Variable Cost Revenue" figure (economic measures pre-defined).  Other options to view the data are point and line graphs.'), tags$br(),
+                 
+             tags$p('The Catch Shares Program was implemented in 2011.  A dashed line wil appear in plots if years pre- and post- implementation of the catch shares program are chosen.
+                  Years prior to the catch shares program are left of the dashed line.'),
+                  
+             tags$p(strong("To get started, make at least one selection in each of the panels on the left."),tags$br(),
                   "Options for changing the chart type and a button to download the plot output can be found beneath the plot."),
            
            tags$p('To view these instructions again at any time visit the "Instructions" tab.')
@@ -19,8 +25,8 @@ output$DefaultTableText <- renderUI({
            tags$h4("Data Table"),
            tags$p('View data used to create the Summary Plot.', tags$br(),
                   'After a data table has been displayed, the data can be further filtered using the "Filter data by" box, 
-                     or filter within a column using the boxes on the bottom of the table. ',tags$br(),
-                  'A button to download the table can be found beneath the table.'),
+                     or filter within a column using the boxes on the bottom of the table. '),
+                 tags$p( strong('A button to download the table can be found beneath the table.')),
            
            tags$p('To view these instructions again at any time visit the "Instructions" tab.')
   )    
@@ -35,7 +41,10 @@ output$DefaultThirdsText <- renderUI({
                     vessels grouped based on their annual revenue: top revenue earners, middle revenue earners, and the bottom revenue earners.  
                      Results are plotted as a dot plot if a single year is selected or a line plot if multiple years are selected. ',  
                     #These thirds are plotted as lines labeled: "Top Third", "Middle Third" and "Bottom Third". 
-                    'A download option can be found below the graph'),
+                  tags$p('The Catch Shares Program was implemented in 2011.   A dashed line wil appear in plots if years pre- and post- implementation of the catch shares program are chosen.
+                  Years prior to the catch shares program are left of the dashed line.'),
+                  
+                   tags$p(strong('A download option can be found below the graph'))),
            tags$p(tags$strong("Note:"), "This feature is still in development and has limited functionality.", tags$br(),
                   'Please note that only a single class of the selected summary variable may be selected at a time. The summary statistic only supports the "Average" summary statistic options.'),
            
