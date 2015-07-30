b2dd3f30d7c4cd6ee7ec954ab1a71b2697875397
Melanie <melanie.harsch@NWCDW01697885.nmfs.local>
2015-07-30 15:00:46 -0700

    changed side bar so that fisheries is broken up into catch shares and not with select all button option

--- external/explorer/explorerSourceFiles/ex.io.sidebar1.r
+++ external/explorer/explorerSourceFiles/ex.io.sidebar1.r
@@ -27,23 +27,82 @@ output$CategorySelect <- renderUI({
 #   )
 })
 
+fish.var <- c("All Catch Share Fisheries","At-sea Pacific whiting","Shoreside Pacific whiting","DTS trawl with trawl endorsement","Non-whiting, non-DTS trawl with trawl endorsement",
+  "Groundfish fixed gear with trawl endorsement","Groundfish fixed gear with fixed gear endorsement",
+  "All Non-Catch Share Fisheries", "Crab","Shrimp","Other fisheries")
+
+Variable <- reactive({
+  dat <- DatMain()
+  if(input$CategorySelect == "Homeport"){
+    variable = factorOrder$port
+  } else if(input$CategorySelect == "State"){
+    variable = factorOrder$state
+  } else if(input$CategorySelect == "Fisheries"){
+    variable = fish.var
+  } else {
+    variable = factorOrder$lengths
+    #       subByCategory <- dat[dat$CATEGORY == input$CategorySelect,] 
+  }
+  return(variable)
+})
 
 
 output$VariableSelect <- renderUI({
   if(!is.null(input$CategorySelect)){
-    tagList(
-      actionButton("selectall", "Select all"),
-      checkboxGroupInput("VariableSelect", "", 
+
+      if(input$CategorySelect == "State"){
+       checkboxGroupInput("VariableSelect", "State", choices = factorOrder$state, selected="")
+      } else if(input$CategorySelect == "Vessel length class"){
+        checkboxGroupInput("VariableSelect",  "Vessel length class", choices=factorOrder$lengths, selected="")
+      } else if(input$CategorySelect == "Homeport"){
+         tagList( 
+        actionButton("selectall", "Select all"),
+         
+        checkboxGroupInput("VariableSelect", "Homeport", choices=factorOrder$port, selected="")
+         )
+      } else if(input$CategorySelect=="Fisheries"){
+        tagList(
+        selectInput("fishCatSelect","", c("Catch share fisheries"="CSF", "Non-catch shares fisheries"="NSF", "All fisheries"="AF"), selected="AF"),
+        conditionalPanel(
+          condition="input.fishCatSelect==AF", 
+         actionButton("selectall2", "Select all")),
+        checkboxGroupInput("VariableSelect", "Select one or more fishery", choices=c("All Catch Share Fisheries","At-sea Pacific whiting","Shoreside Pacific whiting","DTS trawl with trawl endorsement","Non-whiting, non-DTS trawl with trawl endorsement",
+                                                                    "Groundfish fixed gear with trawl endorsement","Groundfish fixed gear with fixed gear endorsement",
+                                                                    "All Non-Catch Share Fisheries", "Crab","Shrimp","Other fisheries"), selected="")
+        
+        )
+        }
+      }
+        
+    #   
+        
+#        if(input$CategorySelect == "Fisheries"){
+#      checkboxGroupInput("VariableSelect", "", 
                       # choices = c("All Catch Share Fisheries","At-sea Pacific whiting","Shoreside Pacific whiting","DTS trawl with trawl endorsement","Non-whiting, non-DTS trawl with trawl endorsement",
                       #             "Groundfish fixed gear with trawl endorsement","Groundfish fixed gear with fixed gear endorsement",
                       #             "All Non-Catch Share Fisheries", "Crab","Shrimp","Other fisheries"),#
-                      choices=Variable()#,
+ #                     choices=Variable()#,
                        #selected = ""
-                    )
-    
-    )
-  }
+#                    )
+#    
+#    )
+#  }
    else return()
+})
+
+observe({
+  if (is.null(input$fishCatSelect)) return()
+    else if(input$fishCatSelect=="CSF"){
+      updateCheckboxGroupInput(session,"VariableSelect", choices=fish.var[1:7], selected=NULL)
+    } else  if(input$fishCatSelect=="NSF"){
+      updateCheckboxGroupInput(session,"VariableSelect", choices=fish.var[8:11], selected=NULL)
+     } else { 
+       tagList(
+      actionButton("selectall2", "Select all"),
+      updateCheckboxGroupInput(session,"VariableSelect", choices=fish.var, selected=NULL)
+  )
+       }
+#  else return ()
 })
 
 observe({
@@ -51,7 +110,16 @@ observe({
   else if (input$selectall%%2 == 0) {
      updateCheckboxGroupInput(session,"VariableSelect", selected=as.character(0)) } 
   else {
-    updateCheckboxGroupInput(session,"VariableSelect", selected=Variable())
+    updateCheckboxGroupInput(session,"VariableSelect", selected=factorOrder$port)
+  }
+})
+
+observe({
+  if (is.null(input$selectall2) || input$selectall2 == 0) return() 
+  else if (input$selectall2%%2 == 0) {
+    updateCheckboxGroupInput(session,"VariableSelect", selected=as.character(0)) } 
+  else {
+    updateCheckboxGroupInput(session,"VariableSelect", selected=fish.var)
   }
 })
 
