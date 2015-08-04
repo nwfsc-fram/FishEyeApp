d88c6cfdf5c3a73dd71d1901ccecf92a1fc2eed0
Melanie <melanie.harsch@NWCDW01697885.nmfs.local>
2015-08-04 12:57:08 -0700

    Added a wrapper function that allows for the facet line titles to wrap

--- external/explorer/explorerSourceFiles/doPlot.R
+++ external/explorer/explorerSourceFiles/doPlot.R
@@ -248,7 +248,7 @@ doPlot <- function(dat, x, y, type){
       panel.grid.major.x = element_line(linetype = "blank"),
       panel.grid.major.y = element_line(color = "#656C70", linetype = "dotted"),
       strip.text = element_text(family = "sans", 
-                                size = 16, color = "grey25", vjust=1),
+                                size = 14, color = "grey25", vjust=1),
       strip.background = element_rect(fill = "lightgrey"),
       axis.ticks = element_blank(),
       axis.title.x = element_blank(),
@@ -260,7 +260,87 @@ doPlot <- function(dat, x, y, type){
       legend.text = element_text(family = "sans", 
         color = "grey25", face = "bold", size = 12),
       legend.title = element_blank())
-    
+ 
+ ##function to wrapping facet labels
+    strwrap_strip_text = function(p, pad=0.05) { 
+      # get facet font attributes
+      th = theme_get()
+      if (length(p$theme) > 0L)
+        th = th + p$theme
+      
+      require("grid")
+      grobs <- ggplotGrob(p)
+      
+      # wrap strip x text
+      if ((class(p$facet)[1] == "grid" && !is.null(names(p$facet$cols))) ||
+          class(p$facet)[1] == "wrap")
+      {
+        ps = calc_element("strip.text.x", th)[["size"]]
+        family = calc_element("strip.text.x", th)[["family"]]
+        face = calc_element("strip.text.x", th)[["face"]]
+        
+        if (class(p$facet)[1] == "wrap") {
+          nm = names(p$facet$facets)
+        } else {
+          nm = names(p$facet$cols)
+        }
+        
+        # get number of facet columns
+        levs = levels(factor(p$data[[nm]]))
+        npanels = length(levs)
+        if (class(p$facet)[1] == "wrap") {
+          cols = n2mfrow(npanels)[1]
+        } else {
+          cols = npanels
+        }
+        
+        # get plot width
+        sum = sum(sapply(grobs$width, function(x) convertWidth(x, "in")))
+        panels_width = par("din")[1] - sum  # inches
+        # determine strwrap width
+        panel_width = panels_width / cols
+        mx_ind = which.max(nchar(levs))
+        char_width = strwidth(levs[mx_ind], units="inches", cex=ps / par("ps"), 
+                              family=family, font=gpar(fontface=face)$font) / 
+          nchar(levs[mx_ind])
+        width = floor((panel_width - pad)/ char_width)  # characters
+        
+        # wrap facet text
+        p$data[[nm]] = unlist(lapply(strwrap(p$data[[nm]], width=width, 
+                                             simplify=FALSE), paste, collapse="\n"))
+      }
+      
+      if (class(p$facet)[1] == "grid" && !is.null(names(p$facet$rows))) {  
+        ps = calc_element("strip.text.y", th)[["size"]]
+        family = calc_element("strip.text.y", th)[["family"]]
+        face = calc_element("strip.text.y", th)[["face"]]
+        
+        nm = names(p$facet$rows)
+        
+        # get number of facet columns
+        levs = levels(factor(p$data[[nm]]))
+        rows = length(levs)
+        
+        # get plot height
+        sum = sum(sapply(grobs$height, function(x) convertWidth(x, "in")))
+        panels_height = par("din")[2] - sum  # inches
+        # determine strwrap width
+        panels_height = panels_height / rows
+        mx_ind = which.max(nchar(levs))
+        char_height = strwidth(levs[mx_ind], units="inches", cex=ps / par("ps"), 
+                               family=family, font=gpar(fontface=face)$font) / 
+          nchar(levs[mx_ind])
+        width = floor((panels_height - pad)/ char_height)  # characters
+        
+        # wrap facet text
+        p$data[[nm]] = unlist(lapply(strwrap(p$data[[nm]], width=width, 
+                                             simplify=FALSE), paste, collapse="\n"))
+      }
+      
+      invisible(p)
+    }   
+    #print(g)
+    g <- strwrap_strip_text(g) #use instead of print(g)
     print(g)
       
    } else plot(0,0,type="n", axes=F, xlab="", ylab="")
