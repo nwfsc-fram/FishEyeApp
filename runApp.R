2ee67fed7b60dd50fd095ceb07eb46033a37dabd
Melanie <melanie.harsch@NWCDW01697885.nmfs.local>
2015-08-20 13:06:19 -0700

    Updated list of packages to include at first run of the app

--- runApp.R
+++ runApp.R
@@ -1,6 +1,6 @@
 # Run the following code the first time you load up the app
 
-list.of.packages <- c("ggplot2", "shiny", "reshape2")
+list.of.packages <- c("ggplot2", "shiny", "reshape2","shinyBS","grid","scales")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -11,5 +11,5 @@ if(length(new.packages)) install.packages(new.packages)
 # Todd's Laptop:
 # setwd("C:/todd.lee/Desktop/FISHEYE/vesselSim_app/")
 # these arguements are not necessary
-shiny::runApp(launch.browser = T, port = 5971)
+#shiny::runApp(launch.browser = T, port = 5971)
 
