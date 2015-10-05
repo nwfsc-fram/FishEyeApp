19f91ee140ff46d457f2cc1cdd602bd9456d3c7e
Melanie <melanie.harsch@NWCDW01697885.nmfs.local>
2015-10-05 13:08:16 -0700

    Added function to add data accessed to datatable download

--- external/serverHead.R
+++ external/serverHead.R
@@ -1,5 +1,4 @@
 years.list <- c("2009", "2010", "2011", "2012")
-# load("data/tables.RData") #depricated
 
 # load("data/tabs.out.RData") ## this is now the following datatables
 load("data/netrevTable.RData")
@@ -15,17 +14,29 @@ pal.netrev <- c("Revenue" = "#66a61e", "Fixed costs" = "#e41a1c",
 pal.thirds <- c("#1b9e77", "#d95f02", "#7570b3")
 
 
-# DEPRICATED -- showing categories as facets, not groups
-# for EDC fishery groups
-    # library(ggtheme) # library(scales)
-  # show_col(stata_pal("s2color")(8))
-# pal.fisheries <- c("At-sea Pacific whiting" = "#1a476f", 
-#                   "Shoreside Pacific whiting" = "#90353b", 
-#                   "DTS trawl with trawl endorsement" = "#55752f", 
-#                   "Non-whiting, non-DTS trawl with trawl endorsement" ="#e37e00",
-#                   "Groundfish fixed gear with trawl endorsement" = "#6e8e84",
-# #                   "Groundfish fixed gear with fixed gear endorsement" = "c10534",
-#                   "Crab" = "#c10534",
-#                   "Other fisheries" = "#938dd2",
-# #                   "Alaska" = "#cac27e"
-#                   "Shrimp" = "#cac27e")
+#======================function to help create table with date===================================#
+rbindCommonCols<-function(x, y){
+  
+  commonColNames = intersect(colnames(x), colnames(y))
+  x = x[,commonColNames]
+  y = y[,commonColNames]
+  
+  colClassesX = sapply(x, class)
+  colClassesY = sapply(y, class)
+  classMatch = paste( colClassesX, colClassesY, sep = "-" )
+  factorColIdx = grep("factor", classMatch)
+  
+  for(n in factorColIdx){ 
+    x[,n] = as.factor(x[,n])
+    y[,n] = as.factor(y[,n])
+  }
+  
+  for(n in factorColIdx){ 
+    x[,n] = factor(x[,n], levels = unique(c( levels(x[,n]), levels(y[,n]) )))
+    y[,n] = factor(y[,n], levels = unique(c( levels(y[,n]), levels(x[,n]) )))  
+  } 
+  
+  res = rbind(x,y)
+  res
+}
+
