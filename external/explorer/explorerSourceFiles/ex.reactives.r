4dfc78975badb1ae0ff897048989af4b695099aa
Melanie <melanie.harsch@NWCDW01697885.nmfs.local>
2015-10-21 10:40:03 -0700

    PermitMessage, updates to allow for "all fisheries" category, updates to error messages

--- external/explorer/explorerSourceFiles/ex.reactives.r
+++ external/explorer/explorerSourceFiles/ex.reactives.r
@@ -19,11 +19,11 @@ DatVars <- reactive({
   dat <- DatMain()
   datVars <- with(dat, 
     list(YEAR = unique(YEAR),
-      SHORTDESCR = factorOrder$shortdescr,
+      SHORTDESCR = c("Revenue","Variable costs","Fixed costs","Variable cost net revenue","Total cost net revenue"),
        CATEGORY = c("Fisheries","Homeport","State","Vessel length class"),
       FISHAK = unique(FISHAK),
-      STAT =  c("Average per vessel","Average per vessel/day","Average per vessel/metric-ton","Summed over all vessels"="Total" ),
-      CS = unique(CS)
+      STAT =  c("Average per vessel","Average per vessel/day","Average per vessel/metric-ton","Summed over all vessels"="Total" )
+     
   ))
 })
 
@@ -31,7 +31,7 @@ DatVars <- reactive({
 Variable <- reactive({
   dat <- DatMain()
     if(input$CategorySelect == "Fisheries"){
-      variable = c("All Catch Share Fisheries","At-sea Pacific whiting","Shoreside Pacific whiting","DTS trawl with trawl endorsement","Non-whiting, non-DTS trawl with trawl endorsement",
+      variable = c("All Fisheries", "All Catch Share Fisheries","At-sea Pacific whiting","Shoreside Pacific whiting","DTS trawl with trawl endorsement","Non-whiting, non-DTS trawl with trawl endorsement",
                    "Groundfish fixed gear with trawl endorsement","Groundfish fixed gear with fixed gear endorsement",
                    "All Non-Catch Share Fisheries", "Crab","Shrimp","Other fisheries")
     } else if(input$CategorySelect == "State"){
@@ -68,7 +68,7 @@ DatSubTable <- reactive({
     
     # order for plotting
     datSub$SHORTDESCR <- factor(datSub$SHORTDESCR, 
-                                levels = factorOrder$shortdescr)
+                                levels = c("Revenue","Variable costs","Fixed costs","Variable cost net revenue","Total cost net revenue"))
   
       if(input$CategorySelect != "Fisheries") {
         datSub <- subset(datSub, CS == input$inSelect)
@@ -79,7 +79,7 @@ DatSubTable <- reactive({
     } else if(input$CategorySelect == "State"){
       datSub$VARIABLE <- factor(datSub$VARIABLE, levels = factorOrder$state)
     } else if(input$CategorySelect == "Fisheries"){
-      datSub$VARIABLE <- factor(datSub$VARIABLE, levels = factorOrder$fisheries)
+      datSub$VARIABLE <- factor(datSub$VARIABLE, levels = c("All Fisheries",factorOrder$fisheries))
     }
     
     return(datSub)
@@ -127,7 +127,11 @@ DatSub <- reactive({
       } else if(input$CategorySelect == "State"){
         datSub$VARIABLE <- factor(datSub$VARIABLE, levels = factorOrder$state)
       } else if(input$CategorySelect == "Fisheries"){
-        datSub$VARIABLE <- factor(datSub$VARIABLE, levels = factorOrder$fisheries)
+        datSub$VARIABLE <- factor(datSub$VARIABLE, levels = c("All Fisheries",factorOrder$fisheries))
+      }
+      
+      if(input$CategorySelect=="Fisheries" & length(input$VariableSelect)>1){
+        datSub <- subset(datSub, datSub$VARIABLE!="Groundfish fixed gear with trawl endorsement"|datSub$YEAR!=2009)
       }
  
       validate(
@@ -152,7 +156,7 @@ DatSub <- reactive({
 # build dcast formula using if controls and using the quoted method in dcast
 DatSub2 <- reactive({
  #!is.null(DatMain()) & 
-  if(input$DodgeSelect == "Derivation of total cost net revenue"){
+  if(input$DodgeSelect == "Composition of total cost net revenue"){
     dat <- DatMain()      
     
  
@@ -179,9 +183,13 @@ DatSub2 <- reactive({
     } else if(input$CategorySelect == "State"){
       datSub2$VARIABLE <- factor(datSub2$VARIABLE, levels = factorOrder$state)
     } else if(input$CategorySelect == "Fisheries"){
-      datSub2$VARIABLE <- factor(datSub2$VARIABLE, levels = factorOrder$fisheries)
+      datSub2$VARIABLE <- factor(datSub2$VARIABLE, levels = c("All Fisheries",factorOrder$fisheries))
     }
-   
+
+        if(input$CategorySelect=="Fisheries" & length(input$VariableSelect)>1){
+      datSub2 <- subset(datSub2, datSub2$VARIABLE!="Groundfish fixed gear with trawl endorsement"|datSub2$YEAR!=2009)
+    }
+    
     validate(
       need(min(datSub2$N)>2,
            '  
@@ -200,7 +208,7 @@ DatSub2 <- reactive({
 
 DatSub3 <- reactive({
   
-  if(input$DodgeSelect == "Derivation of variable cost net revenue"){#!is.null(DatMain()) & 
+  if(input$DodgeSelect == "Composition of variable cost net revenue"){#!is.null(DatMain()) & 
     dat <- DatMain()      
     
     
@@ -227,14 +235,19 @@ DatSub3 <- reactive({
     } else if(input$CategorySelect == "State"){
       datSub3$VARIABLE <- factor(datSub3$VARIABLE, levels = factorOrder$state)
     } else if(input$CategorySelect == "Fisheries"){
-      datSub3$VARIABLE <- factor(datSub3$VARIABLE, levels = factorOrder$fisheries)
+      datSub3$VARIABLE <- factor(datSub3$VARIABLE, levels = c("All Fisheries",factorOrder$fisheries))
     }
+    
+    if(input$CategorySelect=="Fisheries" & length(input$VariableSelect)>1){
+      datSub3 <- subset(datSub3, datSub3$VARIABLE!="Groundfish fixed gear with trawl endorsement"|datSub3$YEAR!=2009)
+    }
+    
     
     validate(
       need(min(datSub3$N)>2,
            '  
-              Sorry, this figure could not be generated as data were suppressed to protect confidentiality. 
-              Data are suppressed when there are not enough observations to protect confidentiality. 
+              We are very sorry, but this figure cannot be generated because of confidentiality requirements. 
+              This happens when there are not enough observations to protect confidentiality. 
               Often, this error can be removed by clicking on the "Include vessels that fished in AK" button.
               There are less vessels that fished solely off the west coast than off the west coast and in Alaska.
               If this does not resolve the issue, please try a different selection of your summary variable (fishery, vessel length, homeport, state) or year.')
@@ -269,30 +282,37 @@ DatSubThirds <- reactive({
     datSub$THIRDS <- factor(datSub$THIRDS,
                             levels = factorOrder$thirds)
     
-    datSub$SHORTDESCR <- factor(datSub$SHORTDESCR, 
-                                levels = factorOrder$shortdescr)
+   # datSub$SHORTDESCR <- factor(datSub$SHORTDESCR, 
+    #                            levels = c("Revenue","Variable costs","Fixed costs","Variable cost net revenue","Total cost net revenue"))
+    datSub$class <- ifelse(datSub$SHORTDESCR=="Revenue", 1,2)
+    datSub$class <- ifelse(datSub$SHORTDESCR=="Fixed costs", 3, datSub$class)
+    datSub$class <- ifelse(datSub$SHORTDESCR=="Variable cost net revenue", 4, datSub$class)
+    datSub$class <- ifelse(datSub$SHORTDESCR=="Total cost net revenue", 5, datSub$class)
+    datSub$titles <-ordered(datSub$class, levels=c(1,2,3,4,5), labels=c("Revenue","Variable costs","Fixed costs", "Variable cost net revenue", "Total cost net revenue")) 
     
     if(input$CategorySelect == "Homeport"){
       datSub$VARIABLE <- factor(datSub$VARIABLE, levels = factorOrder$port)
     } else if(input$CategorySelect == "State"){
       datSub$VARIABLE <- factor(datSub$VARIABLE, levels = factorOrder$state)
     } else if(input$CategorySelect == "Fisheries"){
-      datSub$VARIABLE <- factor(datSub$VARIABLE, levels = factorOrder$fisheries)
+      datSub$VARIABLE <- factor(datSub$VARIABLE, levels = c("All Fisheries",factorOrder$fisheries))
     }
    
     validate(
       need(min(datSub$N)>2,
            '  
-              Sorry, this figure could not be generated as data were suppressed to protect confidentiality. 
-              Data are suppressed when there are not enough observations to protect confidentiality. 
-              Please try a different selection of your summary variable (fishery, vessel length, homeport, state) or year.')
+              We are very sorry, but this figure cannot be generated because of confidentiality requirements. 
+              This happens when there are not enough observations to protect confidentiality. 
+              If looking at Homeports, try selecting "All fisheries" under the "show data summed across these fisheries" box. 
+              If looking at Fisheries,  try selecting different years. In most cases, a suppressed message will appear because of 
+              insufficient number of vessels in 2009, 2010, and/or 2011.')
     ) 
     
      validate(
-       need(input$StatSelect != "Summed over all vessels", 
+       need(input$StatSelect != "Total", 
            '
 
-            Sorry, the variability analysis does not support the "summed over all vessels" stastic.
+            We are very sorry, but the variability analysis does not support the "summed over all vessels" stastic.
             Please select a different statistic.')
      )
     return(datSub)
@@ -317,6 +337,21 @@ PermitPlot <- reactive({
   x
 })
 
+PermitMessage <- reactive({
+  if(!(is.null(input$YearSelect) | is.null(input$CategorySelect) | 
+       is.null(input$VariableSelect) |is.null(input$FishAkSelect) | 
+       is.null(input$StatSelect) | is.null(input$ShortdescrSelect))){
+    if(any(grepl(2009, input$YearSelect))){
+    if(any(grepl("Groundfish fixed gear with trawl endorsement",input$VariableSelect)) #input$selectall2!=0 & input$selectall2%%2==1 input$YearSelect[1]==2009 &
+        ){      
+      x <- TRUE
+    } else {
+      x <- FALSE
+    }
+  } else x <- FALSE
+  } else x <- FALSE
+  x
+})
 
 
 #Download buttons only shows up if PermitPlot()==T
