ab0e9473df718e448c290c94b1945d924a5d119c
Melanie <melanie.harsch@NWCDW01697885.nmfs.local>
2015-11-05 15:47:26 -0800

    Updates to text and appearance

--- external/explorer/explorerSourceFiles/instructions.R
+++ external/explorer/explorerSourceFiles/instructions.R
@@ -1,9 +1,11 @@
 tags$div(style = "margin: 15px; 15px; 30px; width: 60%",
          HTML("<div style='display:inline-block;width:100%;padding:0;line-height: 0.72em; margin-top:5px; margin-bottom:5px;'>
-                     <h3>Instructions</h3></div>"),
+                     <h3>Instructions</h3></div>",
+              '<a class="btn btn-primary", href="Definitions.htm" target="_blank"  style="height:47px;margin: -54px 0px 0px 930px"> Open Instructions <br> in new browser tab</a>'
+         ),
          #tags$h3('Instructions'), 
-          tags$p('To use Net Revenue Explorer, make data selections in each of the fields in the panel on the right. 
-          The panel to make data selections will only appear when you are on the',tags$em('Explore the data'), 'page.
+          tags$p('To use Net Revenue Explorer, make data selections in each of the fields in the panel on the right 
+          (this panel will only appear when you are on the',tags$em('Explore the data'), 'page).
           Output will be automatically generated when each of the fields in the panel has at least one selection.', tags$br(), 
           'A button to download the plot(s) and data table can be found at the bottom of the panel.'),
           tags$p('The ', 
@@ -13,34 +15,41 @@ tags$div(style = "margin: 15px; 15px; 30px; width: 60%",
                    
            tags$li(tags$h4("Visualize Data with Plots")),
            tags$p('Visualize', tags$a(href="http://www.nwfsc.noaa.gov/research/divisions/fram/economic/overview.cfm",'Economic Data Collection (EDC)', target="_blank"), 'summary statistics for revenue, costs and net revenue of catcher vessels that participate in the',
-                  tags$a(href="http://www.nwfsc.noaa.gov/research/divisions/fram/catch_shares.cfm", 'Catch Share program. ', target="_blank"),tags$br(),
+                  tags$a(href="http://www.nwfsc.noaa.gov/research/divisions/fram/catch_shares.cfm", 'Catch Share program. ', target="_blank"),tags$br(),tags$br(),
                 
-                'Plotting options can be found at the bottom of the panel on the right.', tags$br(), 
-                'There are three plot options: 1) economic measures side-by-side, 2) composition of total cost net revenue, and 3) composition of variable cost net revenue. 
+                'Plotting options can be found at the bottom of the panel on the right (panel only shown when on the', tags$em('Explore the Data'), 'page).
+                There are three plot options: 1) economic measures side-by-side, 2) composition of total cost net revenue, and 3) composition of variable cost net revenue. 
                 The', tags$em('Economic measures side-by-side'), 'option is useful for comparing economic measures (revenue, costs, net revenue) across years or fisheries. 
-                Click the', tags$em('Economic measures side-by-side'), 'option under', tags$em('Plot Options'), 'field in the panel on the right. A drop down menu below the', tags$em('Plot Options '),  'allows users to change between bar, point, and line plots.', tags$br(),
-                'The two composition of net revenue plots show costs relative to revenue and the difference between revenue and costs (net revenue). You can examine either Variable Cost Net Revenue (VCNR) or Total Cost Net Revenue (TCNR). 
-                VCRN is revenue minus variable costs and TCNR is revenue minus variable and fixed costs. These plots are stacked bar plots. 
+                Click the', tags$em('Economic measures side-by-side'), 'option under', tags$em('Plot Options'), 'field in the panel on the right. A drop-down menu below the', tags$em('Plot Options '),  'allows you to switch between bar, point, and line plots.'), 
+                tags$p('The two composition of net revenue plots show revenue, costs, and net revenue (revenue minus costs). You can examine either Variable Cost Net Revenue (VCNR) or Total Cost Net Revenue (TCNR). 
+                VCNR is revenue minus variable costs and TCNR is revenue minus variable and fixed costs. These plots are stacked bar plots. 
                 Click the', tags$em('Composition of total cost net revenue'), 'or the', tags$em('Composition of variable cost net revenue'), 
-                'choice under', tags$em('Plot Options'), 'in the panel on the right to see these plots.', tags$br(),
-                'A figure demonstrating how VCNR and TCNR are derived is located on the', tags$em('Explore the data'), 'page under the', tags$em('Visualize Data with Plots'), 'tab.'),
-                tags$p(strong('A note about net revenue:'),  'The', tags$a(href="http://www.nwfsc.noaa.gov/research/divisions/fram/economic/economic_data_forms.cfm", 'EDC forms', target="_blank"), 'attempt to capture only costs that are directly related to vessel fishing operations, and do not include other expenses 
+                'choice under', tags$em('Plot Options'), 'in the panel on the right to see these plots.
+                A figure demonstrating how VCNR and TCNR are derived is located on the', tags$em('Explore the data'), 'page under the', tags$em('Visualize Data with Plots'), 'tab.'),
+                tags$p(strong('A note about net revenue:'),  'The', tags$a(href="http://www.nwfsc.noaa.gov/research/divisions/fram/economic/economic_data_forms.cfm", 'EDC survey forms', target="_blank"), 'attempt to capture only costs that are directly related to vessel fishing operations, and do not include other expenses 
             such as vehicles or office costs that may be related to the fishing business. Therefore, the net revenue reported here is an overestimate of the true net revenue.'),
               tags$br(),
-              tags$li(tags$h4("Data Table")),
+     
+                    tags$li(tags$h4("Data Table")),
               tags$p('View data used to generate the plots created in the', tags$em('Visualize Data'), 'tab.', tags$br(),
                   'After a table has been displayed, the data can be further filtered using the', tags$em('Search'), 'box, or filtered within a column using the boxes at the bottom of the table. 
                   Data used in the', tags$em('Variability Analysis'), 'tab are not available at this time.'),
               tags$br(),
-              tags$li(tags$h4("Variability Analysis")),
+      
+                   tags$li(tags$h4("Variability Analysis")),
               tags$p('Catcher vessels that participate in the Catch Share program span a very broad range in term of the scale of their operations. For instance, the revenue earned ranges from around $10,000 to well over $1,500,000.  
                   The purpose of the', tags$em('Variability Analysis'), 'is to show how this heterogeneity relates to their economic performance. We cannot show the economic performance for each individual vessel due to',
                      tags$a(href = "http://www.nwfsc.noaa.gov/research/divisions/fram/documents/Administration_Operations_Report_2014.pdf",'confidentiality rules', target="_blank"), 
-                  'so we group the vessels into three tiered categories: top revenue earners, middle revenue earners, and bottom revenue earners.
-                      We then calculate the average of the selected statistic (per vessel, vessel/day, or vessel/metric-ton) for vessels within each tiered category.', tags$br(), 
-                  'Results are shown if there are at least three vessels in each group. Results are plotted as a dot plot if a single year is selected or a line plot if multiple years are selected.', tags$br(),
+                  'so we group the vessels into three tiered categories: top, middle, and lower revenue earners.
+                      We then calculate the average of the selected statistic (per vessel, vessel/day, or vessel/metric-ton) for vessels within each tiered category.', tags$br(),tags$br(), 
+                  'Results are shown if there are at least three vessels in each group. Results are plotted as a dot plot if a single year is selected or a line plot if multiple years are selected.', tags$br(),tags$br(),
 
-                  tags$strong("Note:"), 'Only a single class of the selected summary variable (fisheries, homeport, state, vessel length class) may be selected at a time. The', tags$em('Summed over all vessels'), 'statistic option is not currently supported.')
+                  tags$strong("Note:"), 'Only a single class of the selected summary variable (fisheries, homeport, state, vessel length class) may be selected at a time. The', tags$em('Summed over all vessels'), 'statistic option is not currently supported.',
+              tags$br(),
+              tags$br(),
+              tags$br(),
+              tags$br())
+              
           )
 
 )
