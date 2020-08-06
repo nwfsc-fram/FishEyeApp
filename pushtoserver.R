769062175d912789d60d42a886d0b1b72e434560
ashley.vizek <ashley.vizek@noaa.gov>
2020-08-06 12:35:48 -0700

    server push script

---
+++ pushtoserver.R
@@ -0,0 +1,8 @@
+# Development
+pscp -r C:/Users/Ashley.Vizek/Documents/fisheyeapp/PerformanceMetrics/  avizek@nwcrweb.nwfsc.noaa.gov:/srv/shiny-server/fisheye/PerformanceMetrics
+
+pscp -P 22 -r C:/Users/Ashley.Vizek/Documents/fisheyeapp/PerformanceMetrics/  avizek@nwcrweb.nwfsc.noaa.gov:/srv/shiny-server/fisheye/PerformanceMetrics
+# Production
+pscp -r C:/Users/Ashley.Vizek/Documents/fisheyeapp/PerformanceMetrics/  avizek@nwcshiny.nwfsc.noaa.gov:/srv/shiny-server/fisheye/PerformanceMetrics
+
+pscp -P 22 -r C:/Users/Ashley.Vizek/Documents/fisheyeapp/PerformanceMetrics/  avizek@nwcshiny.nwfsc.noaa.gov:/srv/shiny-server/fisheye/PerformanceMetrics
